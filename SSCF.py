6443c9f00e360c5a48f8dee1b456c982b1e68e29
DamonLiu-MBP <liu394000785@sina.com>
2016-12-18 13:40:12 +0800

    为pol文件增加一个读取的方法

--- SSCF.py
+++ SSCF.py
@@ -2,38 +2,37 @@ from util import *
 import numpy as np
 
 A = np.array([
-    [0,1,1,0,0,0],
-    [1,0,1,0,0,0],
-    [1,1,0,1,0,0],
-    [0,0,1,0,1,1],
-    [0,0,0,1,0,1],
-    [0,0,0,1,1,0]])
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [1, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]])
 P = np.array([
-    [0,1,1,2,3,3],
-    [1,0,1,2,3,3],
-    [1,1,0,1,2,2],
-    [2,2,1,0,1,1],
-    [3,3,2,1,0,1],
-    [3,3,2,1,1,0]])
+    [0, 1, 1, 2, 3, 3],
+    [1, 0, 1, 2, 3, 3],
+    [1, 1, 0, 1, 2, 2],
+    [2, 2, 1, 0, 1, 1],
+    [3, 3, 2, 1, 0, 1],
+    [3, 3, 2, 1, 1, 0]])
 F = np.array([
-    [0.00,1.00,0.88,0.00,0.00,0.00],
-    [1.00,0.00,0.88,0.00,0.00,0.00],
-    [0.88,0.88,0.00,0.79,0.00,0.00],
-    [0.00,0.00,0.79,0.00,0.88,0.88],
-    [0.00,0.00,0.00,0.88,0.00,1.00],
-    [0.00,0.00,0.00,0.88,1.00,0.00]])
+    [0.00, 1.00, 0.88, 0.00, 0.00, 0.00],
+    [1.00, 0.00, 0.88, 0.00, 0.00, 0.00],
+    [0.88, 0.88, 0.00, 0.79, 0.00, 0.00],
+    [0.00, 0.00, 0.79, 0.00, 0.88, 0.88],
+    [0.00, 0.00, 0.00, 0.88, 0.00, 1.00],
+    [0.00, 0.00, 0.00, 0.88, 1.00, 0.00]])
 k = 2
 # dataset_path = './data/karate.gml'
-dataset_path = './data/football.gml'
-# dataset_path = './data/polblogs.gml'
-A = get_adjacent_matrix(dataset_path)
+# dataset_path = './data/football.gml'
+dataset_path = './data/polblogs.zip'
+# A = get_adjacent_matrix(dataset_path)
+A = get_adjacent_for_pol()
 P = find_geodesic_distances(A)
 S = find_sim(P)
 print(S)
 F = find_linear_sparse_code(S)
-Es = find_eigen_vectors(F,k)
-Ea = find_eigen_vectors(A,k)
-centroid, labels, inertia = kmeans(Es,Ea = Ea, n_clusters = 2)
-print(centroid, labels, inertia )
-
-
+Es = find_eigen_vectors(F, k)
+Ea = find_eigen_vectors(A, k)
+centroid, labels, inertia = kmeans(Es, Ea=Ea, n_clusters=2)
+print(centroid, labels, inertia)
--- util.py
+++ util.py
@@ -2,13 +2,27 @@ import numpy as np
 from scipy import linalg
 import sklearn.cluster as sc
 from sklearn import linear_model
+from networkx import *
+
+
 # dataset is A
 def test(dataset_path, sigma):
     matrix = get_adjacent_matrix(dataset_path)
     P = find_geodesic_distances(matrix)
     P = np.array(P)
-    S = (np.exp(-(P*P)/(2 * sigma ** 2)))
+    S = (np.exp(-(P * P) / (2 * sigma ** 2)))
     return S
+
+
+import zipfile
+def get_pol_data():
+    path = './data/polblogs.zip'
+    file = zipfile.ZipFile(path)
+    gml = file.read('polblogs.gml').decode()  # read gml data
+    # throw away bogus first line with # from mejn files
+    gml = gml.split('\n')[1:]
+    G = parse_gml(gml)  # parse gml data
+    return G
 
 def find_geodesic_distances(M):
     """
@@ -58,28 +72,37 @@ def find_geodesic_distances(M):
     P = np.array(P)
     return P
 
+
 def print_graph(G):
     for row in G:
         print(row)
 
-import networkx as nx
+
 def get_data_from_file(path):
+    print(path)
     graph = nx.read_gml(path)
     return graph
+
 
 def get_adjacent_matrix(path):
     matrix = nx.to_numpy_matrix(get_data_from_file(path))
     return matrix
-def find_sim(P, sigma = 1):
-    S = np.exp(-(P*P)/(2 * sigma ** 2))
+
+def get_adjacent_for_pol():
+    matrix = nx.to_numpy_matrix(get_pol_data())
+    return matrix
+
+def find_sim(P, sigma=1):
+    S = np.exp(-(P * P) / (2 * sigma ** 2))
     return S
+
 
 def find_linear_sparse_code(S):
     n = np.shape(S)[0]
     F = np.zeros([n, n])
     for i in range(n):
         Sh = np.column_stack((S[:, :i], np.zeros([n, 1]), S[:, i + 1:]))
-        lasso = linear_model.Lasso(alpha = 0.05, fit_intercept = False)
+        lasso = linear_model.Lasso(alpha=0.05, fit_intercept=False)
         lasso.fit(Sh, S[:, i])
         w = lasso.coef_ / sum(lasso.coef_)
         F[i, :] = F[i, :] + w
@@ -88,26 +111,27 @@ def find_linear_sparse_code(S):
         col = F[i, :]
         max_dig.append([np.max(col)])
     F = F / max_dig
-    F = (F + np.transpose(F))/2
+    F = (F + np.transpose(F)) / 2
     return F
 
-def find_eigen_vectors(F,k):
+
+def find_eigen_vectors(F, k):
     ds = [np.sum(row) for row in F]
     D = np.diag(ds)
-    Dn = np.power(np.linalg.matrix_power(D,-1),0.5)
+    Dn = np.power(np.linalg.matrix_power(D, -1), 0.5)
     L = np.identity(len(F)) - (Dn.dot(F)).dot(Dn)
 
     evals, evcts = linalg.eig(L)
-    vals = dict (zip(evals, evcts.transpose()))
+    vals = dict(zip(evals, evcts.transpose()))
     keys = sorted(vals.keys())
     E = np.array([vals[i] for i in keys[1:k]]).transpose()
     return E
 
-def kmeans(Es,Ea = None,n_clusters = 2):
+
+def kmeans(Es, Ea=None, n_clusters=2):
     E = Es
     if Ea is not None:
         E = (np.concatenate((Es.T, Ea.T))).T
     if (np.sum(E.imag)) > 0: print('the egin is complex number')
-    centroid, labels, inertia = sc.k_means(E.real, n_clusters = n_clusters)
+    centroid, labels, inertia = sc.k_means(E.real, n_clusters=n_clusters)
     return centroid, labels, inertia
-
